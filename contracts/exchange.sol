0604593708522201884a475a29faa6ef05538ab6
codeparameter <codeparameter.com@gmail.com>
2023-10-29 19:59:03 +0330

    make it better

--- contracts/exchange.sol
+++ contracts/exchange.sol
@@ -1,18 +1,9 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-interface ParsaTokenInterface{
-    // Parsa uses The ERC-20 token as parent of its token
-    function mint(address account, uint256 value) external ;
-    function balanceOf(address account) external view returns (uint256);
-    function transfer(address to, uint256 value) external returns (bool);
-    function transferFrom(address from, address to, uint256 value) external returns (bool);
-}
+import "lottery.sol";
 
-contract Exchange {
-    
-    address public owner;
-    ParsaTokenInterface public token; 
+contract Exchange is Lottery {
 
     struct ExchangeRequest {
         address user;
@@ -55,27 +46,7 @@ contract Exchange {
         uint256 indexed lastWeiAmount, 
         uint256 indexed lastTokenAmunt);
 
-    // 
-    // 
-    // 
-    //  Handling Token
-    // 
-    // 
-    // 
-
-    function airDrop(address user, uint256 amount) external onlyOwner {
-        // later we can create a lottery
-        token.mint(user, amount);
-    }
-
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Only the owner can call this function");
-        _;
-    }
-
-    constructor(address _token) {
-        owner = msg.sender;
-        token = ParsaTokenInterface(_token);
+    constructor(address _token) Lottery(_token) {
     }
 
 
@@ -232,150 +203,5 @@ contract Exchange {
         return buyerWei;
     }
 
-    // 
-    // 
-    // 
-    //  Handling Buy Requests
-    // 
-    // 
-    // 
-
-    function submitBuyRequest(uint256 tokenAmount, uint256 weiAmount, bool allAtOnce) payable external {
-        require(tokenAmount > 0, "Invalid token amount");
-        require(weiAmount > 0, "Invalid Wei amount");
-        require(msg.value > 0, "Insufficiant Wei amount");
-
-        ExchangeRequest memory buyRequest = ExchangeRequest(
-            msg.sender, 
-            tokenAmount, 
-            weiAmount, 
-            weiAmount / tokenAmount, 
-            allAtOnce
-            );
-
-        uint256 buyRI = storeBuyRequest(buyRequest);
-        matchSellRequest(buyRequest, buyRI);
-    }
-
-    function matchSellRequest(ExchangeRequest memory buyRequest, uint256 buyRI) internal {
-        ExchangeRequest memory sellRequest;
-        for (uint256 i = sellRequests.length - 1; i >= 0; i--){
-            sellRequest = sellRequests[i];
-
-            if(buyRequest.rate < sellRequest.rate)
-                break ;
-            if(sellRequest.allAtOnce && sellRequest.weiAmount > buyRequest.weiAmount)
-                continue ;
-            if(buyRequest.allAtOnce && buyRequest.tokenAmount > sellRequest.tokenAmount)
-                continue ;
-                
-            if(buyRequest.tokenAmount > sellRequest.tokenAmount){
-
-                transferWei(buyRequest, sellRequest, sellRequest.tokenAmount);
-                closeSellRequest(sellRequest, i);
-
-                // transfer to buyer now (without lock)
-                token.transfer(buyRequest.user, sellRequest.tokenAmount);
-                buyRequest = shrinkBuyRequest(
-                    buyRequest, sellRequest.weiAmount, sellRequest.tokenAmount, buyRI);
-
-                if(buyRequest.tokenAmount == 0){
-                    closeBuyRequest(buyRequest, buyRI);
-                    return ;
-                }
-            }
-            else{
-
-                uint256 weiAmount = transferWei(buyRequest, sellRequest, buyRequest.tokenAmount);
-                        
-                if(buyRequest.tokenAmount < sellRequest.tokenAmount)
-                    shrinkSellRequest(sellRequest, weiAmount, buyRequest.tokenAmount, i);
-                else // ==
-                    closeSellRequest(sellRequest, i);
-                
-                // transfer to buyer now (without lock)
-                token.transfer(buyRequest.user, buyRequest.tokenAmount);
-                closeBuyRequest(buyRequest, buyRI);
-
-                return ;
-            }            
-        }
-
-        // lock weis to match later
-        if(buyRequest.tokenAmount > 0)
-            payable(address(this)).transfer(buyRequest.weiAmount);
-    }
-
-    // 
-    // 
-    // 
-    //  Handling Sell Requests
-    // 
-    // 
-    // 
-
-    function submitSellRequest(uint256 tokenAmount, uint256 weiAmount, bool allAtOnce) external {
-        require(tokenAmount > 0, "Invalid token amount");
-        require(weiAmount > 0, "Invalid ETH amount");
-        require(token.balanceOf(msg.sender) > 0, "Insufficiant token amount");
-
-        ExchangeRequest memory sellRequest = ExchangeRequest(
-            msg.sender, 
-            tokenAmount, 
-            weiAmount, 
-            weiAmount / tokenAmount, 
-            allAtOnce
-            );
-
-        uint256 sellRI = storeSellRequest(sellRequest);
-        matchBuyRequest(sellRequest, sellRI);
-    }
-
-    function matchBuyRequest(ExchangeRequest memory sellRequest, uint256 sellRI) internal {
-        ExchangeRequest memory buyRequest;
-        for (uint256 i = buyRequests.length - 1; i >= 0; i--){
-            buyRequest = buyRequests[i];
-
-            if(buyRequest.rate < sellRequest.rate)
-                break ;
-            if(buyRequest.allAtOnce && buyRequest.tokenAmount > sellRequest.tokenAmount)
-                continue ;
-            if(sellRequest.allAtOnce && sellRequest.weiAmount > buyRequest.weiAmount)
-                continue ;
-                
-            if(sellRequest.weiAmount > buyRequest.weiAmount){
-                
-                uint256 weiAmount= transferWei(buyRequest, sellRequest, buyRequest.tokenAmount);
-                token.transferFrom(sellRequest.user, buyRequest.user, buyRequest.tokenAmount);
-                closeBuyRequest(buyRequest, i);
-                
-                sellRequest = shrinkSellRequest(
-                    sellRequest, weiAmount, buyRequest.tokenAmount, sellRI);
-
-                if(sellRequest.weiAmount == 0){
-                    closeSellRequest(sellRequest, sellRI);
-                    return ;
-                }
-            }
-            else{
-
-                uint256 weiAmount = transferWei(buyRequest, sellRequest, sellRequest.tokenAmount);        
-                token.transferFrom(sellRequest.user, buyRequest.user, sellRequest.tokenAmount);
-
-                if(sellRequest.weiAmount < buyRequest.weiAmount)
-                    shrinkBuyRequest(buyRequest, weiAmount, sellRequest.tokenAmount, i);
-                else // ==
-                    closeBuyRequest(buyRequest, i);
-                
-                closeSellRequest(sellRequest, i);
-                
-                return  ;
-            }            
-        }
-
-        // lock tokens to match later
-        if(sellRequest.weiAmount > 0)
-            token.transfer(address(this), sellRequest.tokenAmount);
-    }
 
 }
